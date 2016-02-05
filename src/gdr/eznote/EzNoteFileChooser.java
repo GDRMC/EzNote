69c2fd1a0d2188896574ab53c60336a95d2b5634
GDRMc <gdrdu86@gmail.com>
2016-02-05 02:05:28 +0100

    updated eznotefilechooser for catching window close and prevent file being saved if the window is closed

--- src/gdr/eznote/EzNoteFileChooser.java
+++ src/gdr/eznote/EzNoteFileChooser.java
@@ -1,78 +1,22 @@
 package gdr.eznote;
 
 import java.awt.Component;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowFocusListener;
-import java.awt.event.WindowListener;
-import java.awt.event.WindowStateListener;
 import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 
-public class EzNoteFileChooser extends JFileChooser implements WindowListener, WindowFocusListener, WindowStateListener{
+public class EzNoteFileChooser extends JFileChooser {
 
-    static EzNoteFileChooser fc = new EzNoteFileChooser("test");
-    
-    public EzNoteFileChooser(String name){
+    public EzNoteFileChooser(JFrame parent, String name) {
         super(name);
         this.setDialogTitle("Open/Close");
         this.setVisible(true);
     }
-    
-    public void displayOpen(Component c){
+
+    public void displayOpen(Component c) {
         this.showOpenDialog(c);
     }
-    
-    public void displayClose(Component c){
+
+    public void displayClose(Component c) {
         this.showSaveDialog(c);
     }
-    
-    @Override
-    public void windowOpened(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void windowClosing(WindowEvent e) {
-        System.out.println("test");
-    }
-
-    @Override
-    public void windowClosed(WindowEvent e) {
-        
-    }
-
-    @Override
-    public void windowIconified(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void windowDeiconified(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void windowActivated(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void windowDeactivated(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void windowGainedFocus(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void windowLostFocus(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void windowStateChanged(WindowEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
