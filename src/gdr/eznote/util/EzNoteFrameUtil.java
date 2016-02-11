589d8f4c37ec50a760cd268b3a596637bc0ea231
gdr <gdr@redhairgurlwguns>
2016-02-11 12:54:56 +0100

    new utilities class

---
+++ src/gdr/eznote/util/EzNoteFrameUtil.java
@@ -0,0 +1,15 @@
+package gdr.eznote.util;
+
+import gdr.eznote.*;
+
+public class EzNoteFrameUtil {
+    
+    private EzNoteFrame p;
+    
+    public EzNoteFrameUtil(EzNoteFrame parent){
+        this.p = parent;
+    }
+    
+    
+    
+}
