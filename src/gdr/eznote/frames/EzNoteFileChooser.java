a8475867e4b9a7f9c73bbfddb956e1feb9dac627
gdr <gdr@redhairgurlwguns>
2016-02-11 12:54:40 +0100

    moved subframes here

---
+++ src/gdr/eznote/frames/EzNoteFileChooser.java
@@ -0,0 +1,22 @@
+package gdr.eznote.frames;
+
+import java.awt.Component;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+public class EzNoteFileChooser extends JFileChooser {
+
+    public EzNoteFileChooser(JFrame parent, String name) {
+        super(name);
+        this.setDialogTitle("Open/Close");
+        this.setVisible(true);
+    }
+
+    public void displayOpen(Component c) {
+        this.showOpenDialog(c);
+    }
+
+    public void displayClose(Component c) {
+        this.showSaveDialog(c);
+    }
+}
