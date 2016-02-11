5fc64748a7fcc6f2214e62567235132072e08551
gdr <gdr@redhairgurlwguns>
2016-02-11 17:20:52 +0100

    git branch resolve

--- src/gdr/eznote/EzNoteDocument.java
+++ src/gdr/eznote/EzNoteDocument.java
@@ -13,6 +13,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
+//ed
 public class EzNoteDocument {
     
     private EzNoteDocumentListener dc;
