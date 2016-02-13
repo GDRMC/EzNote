b2b3a837247db94b693c2410c92d9376aa7286f2
GDRMc <gdrdu86@gmail.com>
2016-02-13 03:09:37 +0100

    dev2 Javadoc and Updated deprecated methods

--- src/gdr/eznote/document/EzNoteDocumentListener.java
+++ src/gdr/eznote/document/EzNoteDocumentListener.java
@@ -4,12 +4,20 @@ import gdr.eznote.EzNoteFrame;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+/**
+ * Document Listener class
+ * @author GDRMc
+ */
 public class EzNoteDocumentListener implements DocumentListener{
     
     private int changeCounter;
     private boolean changeIndicator;
     private EzNoteFrame parent;
     
+    /**
+     * Constructor
+     * @param parent parent frame
+     */
     public EzNoteDocumentListener(EzNoteFrame parent){
         super();
         this.changeCounter = 0;
@@ -39,29 +47,68 @@ public class EzNoteDocumentListener implements DocumentListener{
         notifyEvent("event change");
     }
     
+    /**
+     * Prints a string to the console (debug)
+     * Deprecated, this will be removed in some releases
+     * @param str
+     */
+    @Deprecated
     public void notifyEvent(String str){
         System.out.println("EzFrameDL dc > "+str);
     }
 
+    /**
+     * Returns the change counter of the document
+     * @return int counter change
+     */
     public int getChangeCounter() {
         return changeCounter;
     }
 
+    /**
+     * Returns the change indicator of the document
+     * @return boolean change indicator
+     */
     public boolean getChangeIndicator() {
         return changeIndicator;
     }
 
+    /**
+     * Returns the parent window of the listener
+     * @return parent window
+     */
     public EzNoteFrame getParent() {
         return parent;
     }
 
+    /**
+     * Set the change counter of the document
+     * Deprecated: will be replaced by resetChange, this is for
+     * debug only
+     * @param changeCounter
+     */
+    @Deprecated
     public void setChangeCounter(int changeCounter) {
         this.changeCounter = changeCounter;
     }
 
+    /**
+     * Set the change indicator
+     * Deprecated: will be replaced by resetChange, this is for
+     * debug only
+     * @param changeIndicator
+     */
+    @Deprecated
     public void setChangeIndicator(boolean changeIndicator) {
         this.changeIndicator = changeIndicator;
     }
     
+    /**
+     * Resets the changes censors of the document
+     */
+    public void resetIndicators(){
+        this.changeIndicator = false;
+        this.changeCounter = 0;
+    }
     
 }
