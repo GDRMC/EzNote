b7302cad6e16f99cb1cbc519b3996d4b26503b2d
GDRMc <gdrdu86@gmail.com>
2016-02-13 01:32:06 +0100

    dev2 updated frame utilities class

--- src/gdr/eznote/util/EzNoteFrameUtil.java
+++ src/gdr/eznote/util/EzNoteFrameUtil.java
@@ -3,13 +3,43 @@ package gdr.eznote.util;
 import gdr.eznote.*;
 
 public class EzNoteFrameUtil {
-    
+
     private EzNoteFrame p;
-    
-    public EzNoteFrameUtil(EzNoteFrame parent){
+
+    public EzNoteFrameUtil(EzNoteFrame parent) {
         this.p = parent;
     }
-    
-    
+
+    public String getWindowTitle(boolean fileLoaded, String filename) {
+        if (!filename.isEmpty()) {
+            if (fileLoaded) {
+                return "*"
+                        + "<"+filename+">"
+                        + EzNoteTexts.TITLE_SEPARATOR
+                        + EzNoteTexts.SOFTWARE_NAME
+                        + " "
+                        + EzNoteTexts.SOFTWARE_VERSION
+                        + EzNoteTexts.TITLE_SEPARATOR
+                        + EzNoteTexts.TITLE_TEXT1;
+            } else {
+                return "<"+filename+">"
+                        + EzNoteTexts.TITLE_SEPARATOR
+                        + EzNoteTexts.SOFTWARE_NAME
+                        + " "
+                        + EzNoteTexts.SOFTWARE_VERSION
+                        + EzNoteTexts.TITLE_SEPARATOR
+                        + EzNoteTexts.TITLE_TEXT1;
+            }
+        } else {
+            return "<Untitled>"
+                        + EzNoteTexts.TITLE_SEPARATOR
+                        + EzNoteTexts.SOFTWARE_NAME
+                        + " "
+                        + EzNoteTexts.SOFTWARE_VERSION
+                        + EzNoteTexts.TITLE_SEPARATOR
+                        + EzNoteTexts.TITLE_TEXT1;
+        }
+        
+    }
     
 }
