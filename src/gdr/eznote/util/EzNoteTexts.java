35fd54ef942410060c6e4f42bb8e1aa8d99366ae
GDRMc <gdrdu86@gmail.com>
2016-02-13 03:17:39 +0100

    dev2 Javadoc

--- src/gdr/eznote/util/EzNoteTexts.java
+++ src/gdr/eznote/util/EzNoteTexts.java
@@ -1,9 +1,29 @@
 package gdr.eznote.util;
 
+/**
+ * String components for Frames
+ * @author GDRMc
+ */
 public enum EzNoteTexts {
+
+    /**
+     * Software string
+     */
     SOFTWARE_NAME("EzNote"),
+
+    /**
+     * Version number
+     */
     SOFTWARE_VERSION("0.001d"),
+
+    /**
+     * String separator (for title only)
+     */
     TITLE_SEPARATOR(" - "),
+
+    /**
+     * Custom String #1
+     */
     TITLE_TEXT1("Lightweight txt editor"),
     ;
     
