031576d77949164dd995a7206304061c131753b3
GDRMc <gdrdu86@gmail.com>
2016-02-14 17:53:45 +0100

    dev2 merged class with EzNoteUtil

--- src/gdr/eznote/util/EzNoteFrameUtil.java
+++ src/gdr/eznote/util/EzNoteFrameUtil.java
@@ -1,6 +1,7 @@
 package gdr.eznote.util;
 
 import gdr.eznote.*;
+import java.io.File;
 
 public class EzNoteFrameUtil {
 
@@ -8,6 +9,21 @@ public class EzNoteFrameUtil {
 
     public EzNoteFrameUtil(EzNoteFrame parent) {
         this.p = parent;
+    }
+    
+    public static String getProperFileExtension(String absolutePath){
+        if(absolutePath.endsWith("txt")){
+            return absolutePath;
+        } else {
+            return absolutePath+".txt";
+        }
+    }
+    
+    public static void debugFile(File f){
+        System.out.println("Absolute Path: "+f.getAbsolutePath());
+        System.out.println("File Name: "+f.getName());
+        System.out.println("String: "+f.toString());
+        System.out.println("Exists: "+f.exists());
     }
 
     public String getWindowTitle(boolean fileLoaded, String filename) {
