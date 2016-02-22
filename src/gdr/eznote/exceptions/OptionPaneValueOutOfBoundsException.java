4865c4046c2296873596576a3e6cd8d452655317
gdaly <gdrdu86@gmail.com>
2016-02-22 19:53:10 +0100

    dev2 new exception

---
+++ src/gdr/eznote/exceptions/OptionPaneValueOutOfBoundsException.java
@@ -0,0 +1,15 @@
+package gdr.eznote.exceptions;
+
+public class OptionPaneValueOutOfBoundsException extends Exception {
+    
+    public OptionPaneValueOutOfBoundsException(String message) { 
+        super(message); 
+    }
+    public OptionPaneValueOutOfBoundsException(String message, Throwable cause) { 
+        super(message, cause); 
+    }
+    public OptionPaneValueOutOfBoundsException(Throwable cause) { 
+        super(cause); 
+    }
+    
+}
